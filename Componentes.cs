b7d750872c7a258306c5ec6096fd226aabe434a1
kaaly06 <kaliany.palma@etec.sp.gov.br>
2022-09-29 15:58:25 -0300

    First commit

---
+++ Componentes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collection.Generic;
+using System.Text;
+
+//criando classe modelo
+namespace AppGuiaCurso.Models
+{
+	public class Componente
+	{
+		public string Nome { get; set; }
+
+		public int CargaHoraria { get; set; }
+
+		public string AtribuicoesResponsabilidades { get; set; }
+
+		public string ValoresAtitudes { get; set; }
+
+		public string Competencias { get; set; }
+
+
+		public string Habilidades { get; set; }
+
+
+		public string BasesTecnologicas { get; set; }
+
+
+
+	}
+}
+
