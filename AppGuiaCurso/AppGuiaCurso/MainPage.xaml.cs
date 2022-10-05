4c9b689e74f15cafc32fcb019e5382d8306b423f
kaaly06 <kaliany.palma@etec.sp.gov.br>
2022-10-05 15:58:58 -0300

    semifinalizado

--- AppGuiaCurso/AppGuiaCurso/MainPage.xaml.cs
+++ AppGuiaCurso/AppGuiaCurso/MainPage.xaml.cs
@@ -5,14 +5,98 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using AppGuiaCurso.Views;
 
 namespace AppGuiaCurso
 {
-    public partial class MainPage : ContentPage
+
+    [DesignTimeVisible(false)]
+
+    public partial class MainPage : MasterDatailPage
     {
         public MainPage()
         {
             InitializeComponent();
+
+            Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(inicial)));
+        }
+    }
+
+    private async void Open_Inical(object sender, EventArgs e)
+    {
+        try
+        {
+            Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(inicial)));
+            IsPresented = false;
+        }
+        catch (Expection ex)
+        {
+            await DisplayAlert("Ops!", ex.Message, "OK");
+        }
+    }
+
+    private async void Open_Primeiro(object sender, EventArgs e)
+    {
+        try
+        {
+            Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(inicial)));
+            IsPresented = false;
+        }
+        catch (Expection ex)
+        {
+            await DisplayAlert("Ops!", ex.Message, "OK");
+        }
+    }
+
+    private async void Open_Segundo(object sender, EventArgs e)
+    {
+        try
+        {
+            Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(inicial)));
+            IsPresented = false;
+        }
+        catch (Expection ex)
+        {
+            await DisplayAlert("Ops!", ex.Message, "OK");
+        }
+    }
+
+    private async void Open_Terceiro(object sender, EventArgs e)
+    {
+        try
+        {
+            Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(inicial)));
+            IsPresented = false;
+        }
+        catch (Expection ex)
+        {
+            await DisplayAlert("Ops!", ex.Message, "OK");
+        }
+    }
+
+    private async void Open_Vestibular(object sender, EventArgs e)
+    {
+        try
+        {
+            Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(inicial)));
+            IsPresented = false;
+        }
+        catch (Expection ex)
+        {
+            await DisplayAlert("Ops!", ex.Message, "OK");
+        }
+    }
+
+    private async void Open_Contato(object sender, EventArgs e)
+    {
+        try
+        {
+            Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(inicial)));
+            IsPresented = false;
+        }
+        catch (Expection ex)
+        {
+            await DisplayAlert("Ops!", ex.Message, "OK");
         }
     }
 }
